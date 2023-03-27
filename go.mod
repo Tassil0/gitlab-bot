a90286a5f78f27ffb64540c5138327fec2ec13d1
Tassil0 <contact@fsikora.com>
2023-03-27 02:02:51 +0200

    yea

--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module github.com/Tassil0/gitlab-bot.git
 go 1.20
 
 require gopkg.in/telebot.v3 v3.1.3
+
+require github.com/go-playground/webhooks/v6 v6.0.1 // indirect
--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"github.com/go-playground/webhooks/v6/gitlab"
 	"log"
+	"net/http"
 	"os"
 	"time"
 
@@ -17,12 +19,33 @@ func main() {
 	b, err := tele.NewBot(pref)
 	if err != nil {
 		log.Fatal(err)
-		return
 	}
 
 	b.Handle("/hello", func(c tele.Context) error {
-		return c.Send("Hello!")
+		return c.Send(c.Data())
 	})
 
+	hook, _ := gitlab.New()
+	http.HandleFunc("/webhooks", func(w http.ResponseWriter, r *http.Request) {
+		payload, err := hook.Parse(r, gitlab.IssuesEvents, gitlab.MergeRequestEvents)
+		if err != nil {
+			if err == gitlab.ErrEventNotFound {
+				log.Println("Event not supported")
+				w.WriteHeader(http.StatusInternalServerError)
+				return
+			}
+		}
+		switch payload.(type) {
+		case gitlab.IssueEventPayload:
+			//content := payload.(gitlab.IssueEventPayload)
+			//b.Send("", content)
+		}
+		w.WriteHeader(http.StatusOK)
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	b.Start()
+	log.Fatal(http.ListenAndServe(":8980", nil))
 }
